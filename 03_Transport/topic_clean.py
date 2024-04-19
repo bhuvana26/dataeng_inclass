24848009a1ea7907661c72e327650e0ff36f4aed
bhuvana26 <50573942+bhuvana26@users.noreply.github.com>
2024-04-19 13:04:46 -0700

    Update topic_clean.py

--- 03_Transport/topic_clean.py
+++ 03_Transport/topic_clean.py
@@ -8,6 +8,6 @@ for subscription in subscriptions:
     if topic_path in subscription.topic:
         subscription_path = subscription.name
         subscriber.delete_subscription(request={"subscription": subscription_path})
-        print(f"All messages in subscription '{subscription_path}' have been deleted.")
+        print(f"All messages have been deleted in subscription '{subscription_path}'.")
 
-print(f"All messages in topic '{topic_path}' have been deleted.")
+print(f"All messages have been deleted in topic '{topic_path}'.")
