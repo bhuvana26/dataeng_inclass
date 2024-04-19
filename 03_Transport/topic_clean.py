be24879d75c65890b30c3694f386fec0d784accf
bhuvana26 <50573942+bhuvana26@users.noreply.github.com>
2024-04-19 16:49:54 -0700

    Update topic_clean.py

--- 03_Transport/topic_clean.py
+++ 03_Transport/topic_clean.py
@@ -1,13 +1,13 @@
 from google.cloud import pubsub_v1
 project_id = "dataeng-project-420102"
 topic_name = "my-topic"
-subscriber = pubsub_v1.SubscriberClient()
-topic_path = subscriber.topic_path(project_id, topic_name)
-subscriptions = subscriber.list_subscriptions(project=f"projects/{project_id}")
+sub = pubsub_v1.SubscriberClient()
+topic_path = sub.topic_path(project_id, topic_name)
+subscriptions = sub.list_subscriptions(project=f"projects/{project_id}")
 for subscription in subscriptions:
     if topic_path in subscription.topic:
-        subscription_path = subscription.name
-        subscriber.delete_subscription(request={"subscription": subscription_path})
-        print(f"All messages have been deleted in subscription '{subscription_path}'.")
+        sub_path = subscription.name
+        sub.delete_subscription(request={"subscription": sub_path})
+        print(f"All messages have been deleted in subscription '{sub_path}'.")
 
 print(f"All messages have been deleted in topic '{topic_path}'.")
