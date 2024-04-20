3f1d707af1a0dfb3bb8cc84fbbec79c38fb96373
bhuvana26 <50573942+bhuvana26@users.noreply.github.com>
2024-04-19 17:18:14 -0700

    Update createsub.py

--- 03_Transport/createsub.py
+++ 03_Transport/createsub.py
@@ -1,10 +1,10 @@
 from google.cloud import pubsub_v1
 project_id = "dataeng-project-420102"
 topic_name = "my-topic"
-subscription_name = "my-sub_1"
-subscriber = pubsub_v1.SubscriberClient()
-topic_path = subscriber.topic_path(project_id, topic_name)
-subscription_path = subscriber.subscription_path(project_id, subscription_name)
-subscription = subscriber.create_subscription(request={"name": subscription_path, "topic": topic_path})
+subscription_name = "my-sub"
+sub = pubsub_v1.SubscriberClient()
+topic_path = sub.topic_path(project_id, topic_name)
+subscription_path = sub.subscription_path(project_id, subscription_name)
+subscription = sub.create_subscription(request={"name": subscription_path, "topic": topic_path})
 
-print(f"Subscription '{subscription_path}' has been created for topic '{topic_path}'.")
+print(f"Subscription '{subscription_path}' is created for the topic '{topic_path}'.")
