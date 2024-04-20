048490521db963fceefc305c18fa3427fa5d396d
bhuvana26 <50573942+bhuvana26@users.noreply.github.com>
2024-04-19 19:06:02 -0700

    Update publish02.py

--- 03_Transport/publish02.py
+++ 03_Transport/publish02.py
@@ -4,31 +4,27 @@ import pandas as pd
 import requests
 import base64
 from google.cloud import pubsub_v1
+import time 
 
-# Configure logging
+
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-# Google Cloud project and topic information
 project_id = "dataeng-project-420102"
 topic_id = "my-topic"
 topic_path = f"projects/{project_id}/topics/{topic_id}"
-
-# Initialize Google Cloud Pub/Sub client
 publisher = pubsub_v1.PublisherClient()
-
-# Set to store processed vehicle IDs
 processed_vehicle_ids = set()
 
-def get_vehicle_ids_from_csv(filename, column_name):
+def get_vehicle_ids(filename, column_name):
     df = pd.read_csv(filename)
     return df[column_name].tolist()
 
-def get_response_details(vehicle_id):
+def get_response(vehicle_id):
     url = f"https://busdata.cs.pdx.edu/api/getBreadCrumbs?vehicle_id={vehicle_id}"
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Raise exception for non-2xx responses
+        response.raise_for_status()  
         return response.status_code, response.json()
     except requests.RequestException as e:
         logger.error(f"Error fetching data for vehicle ID {vehicle_id}: {e}")
@@ -41,15 +37,16 @@ def publish_to_pubsub(vehicle_id, content):
         future = publisher.publish(topic_path, data=encoded_message_data, vehicle_id=str(vehicle_id))
         future.result()  # Block until the message is published
         logger.info(f"Published message for vehicle ID: {vehicle_id}")
+        time.sleep(0.25)  # Add 250 milliseconds sleep
     except Exception as e:
         logger.error(f"Error publishing message for vehicle ID {vehicle_id}: {e}")
 
 def main():
     filename = 'vehicle_ids - Sheet1.csv'
     column_name = 'Quest'
-    vehicle_ids = get_vehicle_ids_from_csv(filename, column_name)
+    vehicle_ids = get_vehicle_ids(filename, column_name)
     
-    # Take only the first two vehicle IDs
+    # Take only the two vehicle IDs
     vehicle_ids = vehicle_ids[:2]
 
     if not vehicle_ids:
@@ -61,7 +58,7 @@ def main():
             logger.info(f"Data for vehicle ID {vehicle_id} has already been sent. Skipping.")
             continue
 
-        status_code, content = get_response_details(vehicle_id)
+        status_code, content = get_response(vehicle_id)
         if status_code == 200 and content:
             logger.info(f"Vehicle ID: {vehicle_id}, Response Status: {status_code}")
             logger.info(f"Raw data for vehicle ID {vehicle_id}: {content}")
